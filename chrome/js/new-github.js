1c731e0611176c1b79805c69709f9ab431a92527
Mark Tareshawty <tarebyte@gmail.com>
2014-04-23 18:23:44 -0400

    moved branches button to the header

--- chrome/js/new-github.js
+++ chrome/js/new-github.js
@@ -2,11 +2,18 @@ if (document.URL.split(/\//g).length - 1 == 4) {
   var githubRepo = document.URL.split('/')[4];
   var githubUser = document.URL.split('/')[3];
 
+  generateGeoPatternBackground(githubRepo);
+  createAndInsertCompareLink();
+  moveCreateNewFile();
+  moveBranchListing();
+
   [
     '.octicon-repo:before',
     '.author > a > span',
     '.path-divider',
-    '.js-repo-home-link'
+    '.js-repo-home-link',
+    '.fork-flag > .text > a',
+    '.fork-flag'
   ].forEach(whiteOut);
 
   // White text doesn't look good with the gold background
@@ -18,17 +25,14 @@ if (document.URL.split(/\//g).length - 1 == 4) {
     '.overall-summary',
     $('div.tooltipped-s').attr('aria-label', 'Show language statistics'),
     '.breadcrumb > .repo-root',
-    '.breadcrumb > .separator'
+    '.breadcrumb > .separator',
+    '.sunken-menu-separator',
+    '.in-mid-page'
   ].forEach(remove)
-
-  generateGeoPatternBackground();
-  createAndInsertCompareLink();
-  moveCreateNewFile();
 }
 
-function generateGeoPatternBackground() {
-  var repo = $('.js-current-repository').text();
-  var pattern = GeoPattern.generate(repo);
+function generateGeoPatternBackground(githubRepo) {
+  var pattern = GeoPattern.generate(githubRepo);
 
   $('.pagehead').css(
     {
@@ -43,8 +47,7 @@ function createAndInsertCompareLink() {
   var $compareLink = $('.file-navigation > a')
                       .removeClass()
                       .addClass('tooltipped tooltipped-n')
-                      .css(
-                        {
+                      .css({
                         'color':'white',
                         'font-size': '15px',
                         'padding-right': '20px'
@@ -65,6 +68,25 @@ function moveCreateNewFile() {
   $('.js-new-blob-submit').css({'color': 'white', 'padding-left': '5px'});
 }
 
+function moveBranchListing() {
+  $buttonContainer = $('.select-menu').last();
+  $buttonContainer.children().find('i').remove();
+
+  // Clear out uneeded button styles
+  $buttonContainer
+                .children().first()
+                .css({
+                  'color': 'white',
+                  'background': 'none',
+                  'text-shadow': 'none',
+                  'border': 'none',
+                  'font-weight': 'normal',
+                  'font-size': '15px'
+                });
+
+  $listEl = $('<li>').append($buttonContainer);
+  $('.pagehead-actions').prepend($listEl);
+}
+
 function remove(element, index, array) { $(element).remove(); };
 function whiteOut(element, index, array) { $(element).css('color', 'white') };
-
